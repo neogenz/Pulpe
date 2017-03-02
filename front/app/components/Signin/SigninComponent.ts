e2ab94d1503bb8dcb0d227df19318da47e30b2e8
antoinealtmayer <aaltmayer.contact@gmail.com>
2017-03-02 02:18:38 +0100

    Module Signin

--- front/app/components/Signin/SigninComponent.ts
+++ front/app/components/Signin/SigninComponent.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core'
+import {Component} from '@angular/core'
 
 @Component({
-  selector: 'signin-cpm',
-  templateUrl: 'app/components/Signin/SigninView.html'
+    selector: 'signin-cpm',
+    templateUrl: 'app/components/Signin/SigninView.html',
+    styleUrls: ['app/components/Signin/Signin.css']
 })
-export class SigninComponent{
-  
+export class SigninComponent {
+
 }
