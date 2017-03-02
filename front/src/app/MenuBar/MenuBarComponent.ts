29d5a84b1ca77e801a64643644f3c75cabf81770
neogenz <maxime.desogus@gmail.com>
2017-03-02 13:42:56 +0100

    Migration to angular-cli project

--- front/src/app/MenuBar/MenuBarComponent.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'pulpe-menu-bar-cmp',
-    templateUrl: './app/components/MenuBar/MenuBarView.html'
-})
-export class MenuBarComponent {
-}
