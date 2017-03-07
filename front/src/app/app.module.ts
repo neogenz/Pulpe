71827b619a4ba1ea607ecc0edc951ac08d612e0a
neogenz <maxime.desogus@gmail.com>
2017-03-07 23:09:13 +0100

    Fix syntax bug

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { ChartsModule } from 'ng2-charts';
         HomeComponent,
         MenuBarComponent,
         SigninComponent,
-        ProgramComponent
+        ProgramComponent,
         SignupComponent
     ],
     imports: [
--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -4,7 +4,7 @@
 // import { ModuleWithProviders }  from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Home/HomeComponent';
-import { SigninComponent } from './Signin/SigninComponent';
+import { SigninComponent } from './Signin/signin.component';
 import { ProgramComponent } from './program/program.component';
 import { SignupComponent} from './Signup/signup.component';
 
