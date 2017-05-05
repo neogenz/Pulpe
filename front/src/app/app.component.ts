6d0020cf95e258b48050dacbd6742c46356678b6
xSvpeR <aaltmayer.contact@gmail.com>
2017-05-06 00:00:30 +0200

    - Transform navbar into sidebar - Add footer component

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthenticationService} from "./_services/authentication/authentication.service";
+import {Router} from '@angular/router';
+import {ProfileService} from "./profile/profile.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    public profileIsCompleted: boolean = false;
+    public authenticated: boolean = false;
+    private _opened: boolean = false;
+
+    constructor(public auth: AuthenticationService, private router: Router, public profileService: ProfileService) {
+        this.profileIsCompleted = this.profileService.profileIsCompleted();
+        this.authenticated = this.auth.authenticated();
+    }
+
+    public signout(): void {
+        this.auth.signout();
+        this.router.navigateByUrl('');
+        this.authenticated = false;
+        this.profileIsCompleted = false;
+    }
+
+    private _toggleSidebar() {
+        this._opened = !this._opened;
+    }
 }
