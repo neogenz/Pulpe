060b61192a89bdef11c6eb898cbbaeb6954ca016
antoinealtmayer <aaltmayer.contact@gmail.com>
2017-07-04 22:53:48 +0200

    Redirect to accueil view for coach and member.

--- front/src/app/welcome/welcome.component.ts
+++ front/src/app/welcome/welcome.component.ts
@@ -4,23 +4,29 @@ import {Router} from '@angular/router';
 //import {document} from "@angular/platform-browser/src/facade/browser";
 
 @Component({
-  selector: 'pulpe-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+	selector: 'pulpe-welcome',
+	templateUrl: './welcome.component.html',
+	styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit, OnDestroy {
-  win:any = typeof window !== 'undefined' && window || {};
-  constructor(private authService: AuthenticationService, private router: Router) {
-  }
+	win: any = typeof window !== 'undefined' && window || {};
 
-  ngOnInit() {
-    if (this.authService.authenticated()) {
-      this.router.navigateByUrl('/accueil');
-    }
-    this.win.document.body.className = "landing-page";
-  }
+	constructor(private authService: AuthenticationService, private router: Router) {
+	}
 
-  ngOnDestroy() {
-    this.win.document.body.className = "";
-  }
+	ngOnInit() {
+		if (this.authService.authenticated()) {
+			const authenticationProfile = this.authService.getAuthenticationProfileInLocalStorage();
+			if (authenticationProfile.isCoach) {
+				this.router.navigateByUrl('/accueil/coach');
+			} else {
+				this.router.navigateByUrl('/programme');
+			}
+		}
+		this.win.document.body.className = "landing-page";
+	}
+
+	ngOnDestroy() {
+		this.win.document.body.className = "";
+	}
 }
